3fc5c374ab368b714e356d062629086303d9b68e
alcss <fernandoalcalacasas@hotmail.com>
2022-11-28 00:15:27 -0600

    Secado 1 y 2 aún no funcionan bien

--- js/app.js
+++ js/app.js
@@ -44,16 +44,13 @@ function tick() {
 function simulacion()
 {
     if (llegadaDisponible==true)  { //LLEGADA
-        TLlegada=generadorLlegada();
-        TLlegadaTranscurrido=0;
+        TLlegada=generadorLlegada(); TLlegadaTranscurrido=0;
     }
     let DeltaTLlegada = TLlegada-TLlegadaTranscurrido;llegadaDisponible=false;
     if(DeltaTLlegada==0){
         if(filaCobro < LIM_FILA){
-            filaCobro++;llegadaDisponible=true; //AQUÍ, SE ESTA VOLVIENDO NEGATIVO EL TLlegada
-            //POSIBLEMENTE ES PORQUE EN ALGUN MOMENTO LA FILA LLEGA AL MÁXIMO
-            //ENTONCES LA LLEGADA NO SE VUELVE DISPONIBLE
-            //ENTONCES NO SE REINÍCIA a 0
+            filaCobro++;llegadaDisponible=true; //PARECE QUE ENTRA AQUÍ Y SE AUMENTA
+            //PERO DESPUES SE REDUCE EN ALGUNA PARTE
         }
         if(filaCobro==LIM_FILA){ //SI LA FILA ESTÁ LLENA
             llegadaDisponible=true
@@ -61,60 +58,51 @@ function simulacion()
     }
     TLlegadaTranscurrido=(llegadaDisponible==false && filaCobro<LIM_FILA)?TLlegadaTranscurrido+1:TLlegadaTranscurrido;
 
-    if(cobroDisponible==true && filaCobro!=0){ //COBRO
-        TCobro=generadorCobro();
-        TCobroTranscurrido=0;
+    if(cobroDisponible==true){ //COBRO  //NUNCA ENTRAMOS
+        TCobro=generadorCobro(); TCobroTranscurrido=0;
     }
     let DeltaTCobro= TCobro-TCobroTranscurrido;cobroDisponible=false;
-    if(DeltaTCobro==0){
-        if(filaPreEnjuage<LIM_FILA){
+    TCobroTranscurrido=(cobroDisponible==false && filaPreEnjuage<LIM_FILA && filaCobro!=0)?TCobroTranscurrido+1:TCobroTranscurrido;
+    if(DeltaTCobro==0){ //NUNCA ENTRAMOS
+        if(filaPreEnjuage<LIM_FILA && filaCobro!=0){
             filaPreEnjuage++;cobroDisponible=true;
+            filaCobro--;
         }
-        // if(filaCobro==LIM_FILA){TCobroTranscurrido=TCobro;}
-        filaCobro=(filaCobro==0)?filaCobro:filaCobro-1;
     }
-    TCobroTranscurrido=(cobroDisponible==false && filaPreEnjuage<LIM_FILA)?TCobroTranscurrido+1:TCobroTranscurrido; //QUIZÁ
-    //EL ERROR ESTÉ AQUÍ, DE NADA SIRVE CAMBIAR EL VALOR DE TTranscurrido, si luego se sobre escribirá aquí
-
-    if(preenjuagueDisponible==true && filaPreEnjuage!=0){//PREENGUAJE
-        TPreEnjuague=generadorPreEnjuague(); 
-        TPreEnjuagueTranscurrido=0;
+    
+    if(preenjuagueDisponible==true){//PREENGUAJE
+        TPreEnjuague=generadorPreEnjuague(); TPreEnjuagueTranscurrido=0;
     } 
     let DeltaTPreEnjuague= TPreEnjuague-TPreEnjuagueTranscurrido;preenjuagueDisponible=false;
+    TPreEnjuagueTranscurrido=(preenjuagueDisponible==false && filaLavado<LIM_FILA && filaPreEnjuage!=0)?TPreEnjuagueTranscurrido+1:TPreEnjuagueTranscurrido;
     if(DeltaTPreEnjuague==0){
-        if(filaLavado<LIM_FILA){
-            filaLavado++;preenjuagueDisponible=true;
+        if(filaLavado<LIM_FILA  && filaPreEnjuage!=0){
+            filaLavado++;preenjuagueDisponible=true;filaPreEnjuage--;
         }
-        // if(filaLavado==LIM_FILA){ TPreEnjuagueTranscurrido=TPreEnjuague; }
-        filaPreEnjuage=(filaPreEnjuage==0)?filaPreEnjuage:filaPreEnjuage-1;
     }
-    TPreEnjuagueTranscurrido=(preenjuagueDisponible==false && filaLavado<LIM_FILA)?TPreEnjuagueTranscurrido+1:TPreEnjuagueTranscurrido;
 
-    if(lavadoDisponible==true && filaLavado!=0){//LAVADO
-        TLavado=generadorLavado();
-        TLavadoTranscurrido=0;
+    if(lavadoDisponible==true){//LAVADO
+        TLavado=generadorLavado(); TLavadoTranscurrido=0;
     }
     let DeltaTLavado=TLavado-TLavadoTranscurrido;lavadoDisponible=false;
+    TLavadoTranscurrido=(lavadoDisponible==false && filaSecado<LIM_FILA && filaLavado!=0)?TLavadoTranscurrido+1:TLavadoTranscurrido;
     if(DeltaTLavado==0){
-        if(filaSecado<LIM_FILA){
-            filaSecado++;secadoDisponible=true;
+        if(filaSecado<LIM_FILA && filaLavado!=0){
+            filaSecado++;lavadoDisponible=true;filaLavado--;
         }
-        if(filaSecado==LIM_FILA){TLavadoTranscurrido=TLavado;}
-        filaLavado=(filaLavado==0)?filaLavado:filaLavado-1;
     }
-    TLavadoTranscurrido=(lavadoDisponible==false)?TLavadoTranscurrido+1:TLavadoTranscurrido;
-    if(secado1Disponible==true && filaSecado!=0){//SECADO
+    if(secado1Disponible==true){//SECADO 1
         TSecado1=generadorSecado();
         TSecado1Transcurrido=0;
     }
     let DeltaTSecado1=TSecado1-TSecado1Transcurrido; secado1Disponible=false;
-
     if(DeltaTSecado1==0){
         secado1Disponible=true;
         filaSecado=(filaSecado==0)?filaSecado:filaSecado-1;
     }
     TSecado1Transcurrido++;
-    if(secado2Disponible==true && filaSecado!=0){
+
+    if(secado2Disponible==true){ //SECADO 2
         TSecado2=generadorSecado();
         TSecado2Transcurrido=0;
     }
@@ -126,18 +114,35 @@ function simulacion()
     TSecado2Transcurrido++;
 
     //CONSOLES:LOG
-    
+    document.getElementById("TLlegada").innerHTML="Llegada: "+TLlegada+" seg";
     document.getElementById("Siguete_Llegada").innerHTML="Siguente Llegada en: "+DeltaTLlegada+" seg";
-    document.getElementById("Llegada_lavado").innerHTML="Llegada: "+TLlegada+" seg";
     document.getElementById("Llegada_transcurrido").innerHTML="Tiempo Transcurrido: "+TLlegadaTranscurrido+" seg";
 
-    document.getElementById("Siguiente_Caja").innerHTML="Siguente Caja en: "+ DeltaTCobro+" seg";
-    document.getElementById("value_fila_caja").innerHTML="Fila en la caja: "+filaCobro+" seg";
 
+    document.getElementById("TCaja").innerHTML="Caja: "+TCobro+" seg";
+    document.getElementById("Siguiente_Caja").innerHTML="Siguente Caja en: "+ DeltaTCobro+" seg";
+    document.getElementById("Cobro_transcurrido").innerHTML="Tiempo Transcurrido: "+TCobroTranscurrido+" seg";
+    document.getElementById("value_fila_caja").innerHTML="Fila: "+filaCobro+" autos";
+
+    document.getElementById("Pre-Enjuague").innerHTML="Pre Enjuague: "+TPreEnjuague+" seg";
     document.getElementById("Siguiente_pre_enjuague").innerHTML="Siguente Pre-Enjuague en: "+DeltaTPreEnjuague+" seg";
-    document.getElementById("value_fila_pre_enjuague").innerHTML="Fila en pre_enjuague: "+filaPreEnjuage+" seg";
+    document.getElementById("Pre_Enjuague_transcurrido").innerHTML="Tiempo Transcurrido: "+TPreEnjuagueTranscurrido+" seg";
+    document.getElementById("value_fila_pre_enjuague").innerHTML="Fila: "+filaPreEnjuage+" autos";
+
+    document.getElementById("TLavado").innerHTML="Lavado: "+TLavado+" seg";
+    document.getElementById("Siguiente_Lavado").innerHTML="Siguente Lavado en: "+DeltaTLavado+" seg";
+    document.getElementById("Lavado_transcurrido").innerHTML="Tiempo Transcurrido: "+TLavadoTranscurrido+" seg";
+    document.getElementById("value_fila_lavado").innerHTML="Fila: "+filaLavado+" autos";
     
-    
+    document.getElementById("TSecado1").innerHTML="Secado 1: "+TSecado1+" seg";
+    document.getElementById("Siguiente_Secado1").innerHTML="Siguente Lavado en: "+DeltaTSecado1+" seg";
+    document.getElementById("Secado1_transcurrido").innerHTML="Tiempo Transcurrido: "+TSecado1Transcurrido+" seg";
+    document.getElementById("value_fila_secado1").innerHTML="Fila: "+filaSecado+" autos";
+
+    document.getElementById("TSecado2").innerHTML="Secado 2: "+TSecado2+" seg";
+    document.getElementById("Siguiente_Secado2").innerHTML="Siguente Lavado en: "+DeltaTSecado2+" seg";
+    document.getElementById("Secado2_transcurrido").innerHTML="Tiempo Transcurrido: "+TSecado2Transcurrido+" seg";
+    document.getElementById("value_fila_secado2").innerHTML="Fila: "+filaSecado+" autos";
     //console.log();
     //break;
     document.getElementById("Tiempo_Transcurrido").innerHTML="Tiempo Transcurrido: "+tiempo;
